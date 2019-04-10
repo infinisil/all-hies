8e6a43ed2d9614427ef60051bc52beb2617bb93e
Silvan Mosberger <infinisil@icloud.com>
2019-04-10 02:55:19 +0200

    default.nix: Refactor big time, not quite done yet though

--- default.nix
+++ default.nix
@@ -1,111 +1,153 @@
-with import <nixpkgs/lib>;
-with builtins;
+# nixpkgs used only for library functions, not for dependencies
+{ pkgs ? import ./nixpkgs.nix
+, lib ? pkgs.lib
+}:
 
 let
 
+  # A la lib.composeExtensions, but with any number of extensions
+  composeMultiple = extensions:
+    if extensions == [] then self: super: {}
+    else lib.composeExtensions
+      (lib.head extensions)
+      (composeMultiple (lib.tail extensions));
 
-  pkgsForGhc = mapAttrs (ghc: _: let
-    rev = readFile (./nixpkgsForGhc + "/${ghc}");
-    sha256 = readFile (./nixpkgsHashes + "/${rev}");
-  in
-    import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  # Speeds up Haskell builds
+  speedierBuilds = self: super: {
+    mkDerivation = args: super.mkDerivation (args // {
+      enableLibraryProfiling = false;
+    });
+  };
+
+  ghcSpecific = ghcVersion: rec {
+
+    # Reformats the ghc version into the format "8.6.4"
+    versionString = lib.concatStringsSep "."
+      (builtins.match "ghc(.)(.)(.)" ghcVersion);
+
+    # Evaluates to a nixpkgs that has the given ghc version in it
+    pkgs = let
+      rev = builtins.readFile (./nixpkgsForGhc + "/${ghcVersion}");
+      sha256 = builtins.readFile (./generated/nixpkgsHashes + "/${rev}");
+    in import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/tarball/${rev}";
       inherit sha256;
-    }) {
-      config = {};
-      overlays = [];
-    })
-    (readDir ./nixpkgsForGhc);
-    
-  version = name:
-    mapAttrs' (file: _: let
-      ghcVersion = removeSuffix ".nix" file;
-      pkgs = pkgsForGhc.${ghcVersion};
-      inherit (pkgs) lib haskell;
+    }) { config = {}; overlays = []; };
 
-      overrideFun = old: {
-        overrides = lib.composeExtensions
-          (lib.composeExtensions
-            (old.overrides or (self: super: {}))
-            (hself: hsuper: {
+    # Returns a Haskell overlay that sets all ghc base libraries to null
+    # (minus a select few)
+    baseLibraryNuller = self: super: let
+      libs = lib.splitString " "
+        (builtins.readFile (./generated/ghcBaseLibraries + "/${ghcVersion}"));
+      libNames = map (lib: (builtins.parseDrvName lib).name) libs;
+      # It seems that some versions require Cabal and some don't
+      filtered = lib.filter (name: ! lib.elem name [ "ghc" "Cabal" ]) libNames;
+    in lib.genAttrs filtered (name: null);
 
-              # Disable library profiling for faster builds
-              mkDerivation = args: hsuper.mkDerivation (args // {
-                enableLibraryProfiling = false;
-              });
+    # Custom overrides for specific ghc versions, declared in ./overrides
+    customOverrides =
+      let path = ./overrides + "/${ghcVersion}.nix";
+      in if builtins.pathExists path then import path
+      else self: super: {};
+  };
 
-              # Embed the ghc version into the name
-              haskell-ide-engine = haskell.lib.overrideCabal hsuper.haskell-ide-engine (old: {
-                pname = "${old.pname}-${ghcVersion}";
-                version = substring 0 8 (readFile (./versions + "/${name}/revision"));
-              });
-            }
-            // lib.flip genAttrs (name: null)
-              (lib.filter (name: name != "ghc" && name != "Cabal")
-                (map (name: (parseDrvName name).name)
-                  (lib.splitString " "
-                    (readFile (./ghcBaseLibraries + "/${ghcVersion}")))))
-            )
-          )
-          (if pathExists (./overrides + "/${ghcVersion}.nix") then
-            import (./overrides + "/${ghcVersion}.nix")
-            else self: super: {}
-          );
-      };
 
-      build = haskell.lib.justStaticExecutables
-        ((import (./versions + "/${name}/${file}") {
-          inherit pkgs;
-        }).override overrideFun).haskell-ide-engine;
-    in {
-      name = ghcVersion;
-      value = build;
-    }) 
-    (filterAttrs (file: _: file != "revision") (readDir (./versions + "/${name}")));
+  # Build for a specific GHC version
+  hieBuild = ghcVersion: let
+    forGhc = ghcSpecific ghcVersion;
+    hlib = forGhc.pkgs.haskell.lib;
+    revision = builtins.readFile ./generated/stack2nix/revision;
 
-  versions = mapAttrs (file: _: version file) (readDir ./versions);
+    # Embed the ghc version into the name
+    changeName = self: super: {
+      haskell-ide-engine = hlib.overrideCabal super.haskell-ide-engine (old: {
+        pname = "${old.pname}-${ghcVersion}";
+        version = lib.substring 0 8 revision;
+      });
+    };
 
-  pkgs = import <nixpkgs> {};
-  lib = pkgs.lib;
+    overrideFun = old: {
+      overrides = composeMultiple [
+        (old.overrides or (self: super: {}))
+        speedierBuilds
+        changeName
+        forGhc.baseLibraryNuller
+        forGhc.customOverrides
+      ];
+    };
 
-  parseNixGhcVersion = version:
-    lib.concatStringsSep "." (match "ghc(.)(.)(.)" version);
+    expr = import (./generated/stack2nix + "/${ghcVersion}.nix") {
+      pkgs = forGhc.pkgs;
+    };
 
-  combine = versions: let
+    build = hlib.justStaticExecutables
+      (expr.override overrideFun).haskell-ide-engine;
+
+  in build;
+
+  # A set of all ghc versions for all hie versions, like
+  # { stable = { ghc864 = <derivation ..>; ... }; unstable = ...; }
+  allVersions =
+    let ghcVersionFiles = lib.filterAttrs (file: _: file != "revision")
+      (builtins.readDir ./generated/stack2nix);
+    in lib.mapAttrs' (ghcVersionFile: _:
+
+      let ghcVersion = lib.removeSuffix ".nix" ghcVersionFile;
+      in lib.nameValuePair ghcVersion (hieBuild ghcVersion)
+
+    ) ghcVersionFiles;
+
+  # Combined a set of HIE versions (given as { <ghcVersion> = <derivation>; })
+  # into a single derivation which has a binary hie-$major.$minor.$patch for
+  # every GHC version, and binaries hie and hie-wrapper containing a binary that
+  # automatically selects the correct HIE version out of the available ghc
+  # versions
+  combined = nameSuffix: versions: let
+
+    # This shouldn't matter, but let's use the hie-wrapper binary from the most
+    # recent GHC version
     wrapperVersion = lib.last (lib.attrNames versions);
-  in pkgs.runCommand "haskell-ide-engine-combined" {
+
+  in pkgs.runCommandNoCC "haskell-ide-engine-${nameSuffix}" {
     nativeBuildInputs = [ pkgs.makeWrapper ];
   } ''
     mkdir -p $out/bin
 
-    ${concatMapStringsSep "\n" (ghcVersion: ''
-      ln -s ${versions.${ghcVersion}}/bin/hie $out/bin/hie-${parseNixGhcVersion ghcVersion}
+    ${lib.concatMapStringsSep "\n" (ghcVersion: ''
+      ln -s ${versions.${ghcVersion}}/bin/hie $out/bin/hie-${(ghcSpecific ghcVersion).versionString}
     '') (lib.attrNames versions)}
 
     makeWrapper ${versions.${wrapperVersion}}/bin/hie-wrapper $out/bin/hie-wrapper \
       --suffix PATH : $out/bin
+
     ln -s hie-wrapper $out/bin/hie
   '';
 
-  inherit (versions) stable unstable;
+  #inherit (versions) stable unstable;
 
-  makeSet = versions: combine versions // rec {
-    inherit versions;
-    select = selector: makeSet (selector versions);
-    minors = mapAttrs (name: makeSet)
-      (foldl' (acc: el: let minor = lib.substring 0 (lib.stringLength el - 1) el; in 
-        acc // {
-          ${minor} = acc.${minor} or {} // { ${el} = versions.${el}; };
-        }
-      ) {} (lib.attrNames versions));
-    from = mapAttrs (lower: _: makeSet (filterAttrs (version: _: versionAtLeast version lower) versions)) versions;
-    to = mapAttrs (upper: _: makeSet (filterAttrs (version: _: versionAtLeast upper version) versions)) versions;
+  #makeSet = versions: combine versions // rec {
+  #  inherit versions;
+  #  latest = versions.${last (attrNames versions)};
+  #  select = selector: makeSet (selector versions);
+  #  minors = mapAttrs (name: makeSet)
+  #    (foldl' (acc: el: let minor = lib.substring 0 (lib.stringLength el - 1) el; in 
+  #      acc // {
+  #        ${minor} = acc.${minor} or {} // { ${el} = versions.${el}; };
+  #      }
+  #    ) {} (lib.attrNames versions));
+  #  from = mapAttrs (lower: _: makeSet (filterAttrs (version: _: versionAtLeast version lower) versions)) versions;
+  #  to = mapAttrs (upper: _: makeSet (filterAttrs (version: _: versionAtLeast upper version) versions)) versions;
+  #};
+
+in {
+
+  inherit combined allVersions;
+
+  selection = { selector }: combined "selection"
+    (selector (builtins.intersectAttrs (lib.functionArgs selector) allVersions));
+
+  combos = {
+    all = combined "all" allVersions.stable;
   };
 
-  result = makeSet (unstable // stable) // {
-    onlyStable = makeSet stable;
-    onlyUnstable = makeSet unstable;
-    allVersions = versions;
-  };
-
-in result
+}
