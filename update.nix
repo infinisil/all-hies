17ffe95c4794b6614c9e51f2a0d4ae4577c576ee
Silvan Mosberger <infinisil@icloud.com>
2019-05-06 18:03:57 +0200

    Wrap exe with needed binaries

--- update.nix
+++ update.nix
@@ -3,10 +3,22 @@
 let
   inherit (pkgs) lib;
   hpkgs = pkgs.haskellPackages;
-  pkg = hpkgs.callCabal2nix "all-hies" (pkgs.lib.sourceByRegex ./. [
+  path = lib.makeBinPath [
+    pkgs.nix-prefetch-scripts
+    pkgs.git
+  ];
+  pkg = (hpkgs.callCabal2nix "all-hies" (pkgs.lib.sourceByRegex ./. [
     "update.hs"
     "all-hies.cabal"
-  ]) {};
+  ]) {}).overrideAttrs (old: {
+    nativeBuildInputs = old.nativeBuildInputs or [] ++ [
+      pkgs.makeWrapper
+    ];
+    postInstall = old.postInstall or "" + ''
+      wrapProgram $out/bin/update \
+        --set PATH "${path}"
+    '';
+  });
 in pkg // {
   env = pkg.env.overrideAttrs (old: {
     nativeBuildInputs = old.nativeBuildInputs or [] ++ [
