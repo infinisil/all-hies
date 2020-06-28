035a83650d47431cd6107daaeee416b31d878da7
Silvan Mosberger <contact@infinisil.com>
2020-06-28 02:05:08 +0200

    Update templates

--- templates/nixpkgs-infra/default.nix
+++ templates/nixpkgs-infra/default.nix
@@ -3,10 +3,23 @@ let
     url = "https://github.com/NixOS/nixpkgs/tarball/22a81aa5fc15b2d41b12f7160a71cd4a9f3c3fa1";
     sha256 = "14gx5fsqibdn2cxp7gymfrz2vcnwiwwjnxqlnysczz8dqihnrpa7";
   };
+
+  all-hies = ../..;
+
+  # Use this version for your project instead
+  /*
+  all-hies = fetchTarball {
+	  # Insert the desired all-hies commit here
+    url = "https://github.com/input-output-hk/haskell.nix/tarball/000000000000000000000000000000000000000";
+		# Insert the correct hash after the first evaluation
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+  */
+
   pkgs = import nixpkgs {
     config = {};
     overlays = [
-      (import ../../overlay.nix)
+      (import all-hies {}).overlay
     ];
   };
   inherit (pkgs) lib;
